756a24796a28ab0462c843ec4e3776a289a7f0b1
Maxim Rioux <maxrioux100@gmail.com>
2022-07-17 01:52:50 +0000

    Resolve "Ajouter des données pour les achats"

--- plugin/content-script.js
+++ plugin/content-script.js
@@ -52,7 +52,135 @@ function median(values){
   return bestRound((sorted_values[half - 1] + sorted_values[half]) / 2.0, 6);
 }
 
-function updateGraph() {
+function getNoOutliers(someArray) {  
+    var values = [...someArray];
+   
+    var q1 = values[Math.floor((values.length / 4))];
+    // Likewise for q3. 
+    var q3 = values[Math.ceil((values.length * (3 / 4)))];
+    var iqr = q3 - q1;
+
+    // Then find min and max values
+    maxValue = q3 + iqr*1.5;
+
+    // Then filter anything beyond or beneath these values.
+    var filteredValues = values.filter(function(x) {
+        return (x <= maxValue);
+    });
+
+    // Then return
+    return filteredValues.length;
+}
+
+function updateOffers() {
+	let offers = Array.prototype.slice.call(document.getElementsByClassName("EditionsItem-sc-11cpe2k-7")).splice(1, );
+	
+	let values_eth = [offers.length];
+	let values_dollars = [offers.length];
+	let quantities = [offers.length];
+	
+	for (let i=0; i < offers.length; i++) {
+		let transaction = offers[i].getElementsByClassName("EthPriceLabel-sc-1c1tt50-1")[0].textContent;
+		values_eth[i] = parseFloat(offers[i].getElementsByClassName("EthPriceLabel-sc-1c1tt50-1")[0].textContent.split(' ')[0].replace(',', ''));
+		values_dollars[i] = parseFloat(offers[i].getElementsByClassName("UsdPriceLabel-sc-1c1tt50-2")[0].textContent.split(' ')[0].slice(1,).replace(',', ''));
+		quantities[i] = parseInt(offers[i].getElementsByClassName("EditionsQuantity-sc-11cpe2k-11")[0].textContent);
+	}	
+	
+	let noOutliers = getNoOutliers(values_eth);
+	
+	var new_values_eth = [];
+	var new_values_dollars = [];
+	var new_quantities = [];
+	
+	let lastValue = -1;
+	for (let i=0; i < noOutliers; i++){
+		if (lastValue == values_eth[i]) {
+			new_quantities[new_quantities.length-1] += quantities[i];
+		} else {
+			if (i==0) {new_quantities.push(quantities[i])}
+			else {new_quantities.push(quantities[i] + new_quantities[new_quantities.length-1])};
+			lastValue = values_eth[i];
+			new_values_eth.push(values_eth[i]);
+			new_values_dollars.push(values_dollars[i]);
+		}
+	}
+	
+	let container = document.getElementsByClassName("ContentContainer-sc-1p3n06p-4")[0];
+		let div = document.createElement('div');
+		
+		div.innerHTML = '<header class="SectionTitle-sc-13gqei4-5 hiQCYL">' +
+							'<p class="sc-bkkeKt vhTUk">Offers helper</p>' +
+						'</header>' + 
+						'<section>' +
+							'<div id="chart2"></div>' +
+						'</section>';
+		div.setAttribute('class', 'ContentContainerDesktop-sc-1p3n06p-5 eVGMue'); 
+		container.appendChild(div);	
+		
+		var options2 = {
+			chart: {
+				type: 'line',
+				animations: {
+					enabled: false
+				}
+			},
+			series: [{
+				name: 'Ethereum',
+				data: new_quantities.map(function(e, i) { return [e, new_values_eth[i]]; })
+			},{
+				name: 'Dollars',
+				data: new_quantities.map(function(e, i) { return [e, new_values_dollars[i]]; })
+			}],
+			stroke: {
+			  curve: 'stepline',
+			  width: 1
+			},
+			xaxis: {
+				title: {
+					text: "Number of copies to buy"
+				}
+			},
+			yaxis: [
+			    {
+					title: {
+						text: "Ethereum"
+					},
+					max: new_values_eth[new_values_eth.length-1]
+				},
+				{
+					opposite: true,
+					title: {
+						text: "Dollars"
+					},
+					max: new_values_dollars[new_values_dollars.length-1],
+					decimalsInFloat: 2
+				}
+			],
+			legend: {
+				show: false
+			},
+			colors: [
+				"#008FFB"
+			],
+			tooltip: {
+				custom: function({ series, seriesIndex, dataPointIndex, w }) {
+					return (
+						'<div class="arrow_box">' +
+							"<span>" +
+								`${new_values_eth[dataPointIndex]} ETH (${new_values_dollars[dataPointIndex]}$)` +
+							"</span>" +
+						"</div>"
+					);
+				}
+			}
+		}
+
+		var chart2 = new ApexCharts(document.querySelector("#chart2"), options2);
+
+		chart2.render();
+}
+
+function updateHistory() {
 	let histories = Array.prototype.slice.call(document.getElementsByClassName("HistoryItemWrapper-sc-13gqei4-0"));
 	if (histories.length > 2) {
 		
@@ -79,8 +207,8 @@ function updateGraph() {
 			labels[histories.length - 1 - i] = `<b>${transaction[0]}</b> bought from <b>${transaction[3]}</b>`;
 			values_eth[histories.length - 1 - i] = parseFloat(transaction[5].replace(',', ''));
 			values_dollars[histories.length - 1 - i] = parseFloat(transaction[7].replace(',', '').substring(2,transaction.length-1));
-			total_eth += parseFloat(transaction[5]);
-			total_dollars += parseFloat(transaction[7].substring(2,transaction.length-1));
+			total_eth += values_eth[histories.length - 1 - i];
+			total_dollars += values_dollars[histories.length - 1 - i];
 			if (values_eth[histories.length - 1 - i] < min_eth) { min_eth = values_eth[histories.length - 1 - i]; }
 			if (values_dollars[histories.length - 1 - i] < min_dollars) { min_dollars = values_dollars[histories.length - 1 - i]; }
 			if (values_eth[histories.length - 1 - i] > max_eth) { max_eth = values_eth[histories.length - 1 - i]; }
@@ -90,9 +218,11 @@ function updateGraph() {
 		
 		let first_history = histories[histories.length-1].textContent.replace(")", ") ").split(' ')
 		let last_history = histories[0].textContent.replace(")", ") ").split(' ')
+		let ago_first = first_history.findIndex((element) => element === 'ago');
+		let ago_last = last_history.findIndex((element) => element === 'ago');
+		
 		let container = document.getElementsByClassName("ContentContainer-sc-1p3n06p-4")[0];
 		let div = document.createElement('div');
-		let ago_index = first_history.findIndex((element) => element === 'ago');
 		
 		div.innerHTML = '<header class="SectionTitle-sc-13gqei4-5 hiQCYL">' +
 							'<p class="sc-bkkeKt vhTUk">History helper</p>' +
@@ -100,8 +230,8 @@ function updateGraph() {
 						'<section class="Details-sc-asex48-0 ceZikd">' +				
 							writeChip('Transactions', histories.length) +
 							writeChip('All transactions?', all_transactions) +
-							writeChip('First transaction', `${first_history[ago_index - 2]} ${first_history[ago_index - 1]} ago`) +
-							writeChip('Last transaction', `${last_history[ago_index - 2]} ${last_history[ago_index - 1]} ago`) +
+							writeChip('First transaction', `${first_history[ago_first - 2]} ${first_history[ago_first - 1]} ago`) +
+							writeChip('Last transaction', `${last_history[ago_last - 2]} ${last_history[ago_last - 1]} ago`) +
 							writeChip('Average', `${bestRound(total_eth/histories.length, 3)} ETH (${bestRound(total_dollars/histories.length, 2)}$)`) +
 							writeChip('Median', `${median(values_eth)} ETH (${median(values_dollars)}$)`) +
 							writeChip('Min', `${min_eth} ETH (${min_dollars}$)`) +
@@ -189,10 +319,19 @@ new MutationObserver(() => {
   }
 }).observe(document, {subtree: true, childList: true});
 
+var offers_spawned = false;
+
 function onUrlChange() {
-  if (lastUrl.startsWith("https://nft.gamestop.com/token/")) {
-	  waitForElement(".HistoryItemWrapper-sc-13gqei4-0", 10000).then(updateGraph);
-  }
+	if (lastUrl.startsWith("https://nft.gamestop.com/token/")) {
+		waitForElement(".HistoryItemWrapper-sc-13gqei4-0", 10000)
+		.then(updateHistory);
+		if (!offers_spawned) {
+			offers_spawned = true
+			waitForElement(".EditionsItem-sc-11cpe2k-7")
+			.then(updateOffers)
+			.then(() => offers_spawned = false);
+		}
+	}
 }
 
 onUrlChange();
