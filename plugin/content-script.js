2af560c47676782e3da64193da191f8deed740ed
Kurt Heiden <kurt.w.heiden@gmail.com>
2022-07-17 22:32:10 -0700

    Revert "Adding listener for "Editions" button"
    This reverts commit 7f2527114c0abd5de5666796fe9b1138045203f3.

--- plugin/content-script.js
+++ plugin/content-script.js
@@ -73,11 +73,6 @@ function getNoOutliers(someArray) {
 }
 
 function createOffersHelperContainer() {
-  let editions = document.querySelectorAll("[class^='ButtonHoverWrapper']")[1];
-  if (editions == undefined) {
-    return
-  }
-
 	let container = document.getElementsByClassName("ContentContainer-sc-1p3n06p-4")[0];
 	let div = document.createElement('div');
 
@@ -334,7 +329,6 @@ function onUrlChange() {
 	if (lastUrl.startsWith("https://nft.gamestop.com/token/")) {
 		waitForElement(".HistoryItemWrapper-sc-13gqei4-0", 10000)
 		.then(updateHistory)
-    waitForElement("[class^='ButtonHoverWrapper']", 10000)
 		.then(createOffersHelperContainer);
 		if (!offers_spawned) {
 			offers_spawned = true
@@ -346,3 +340,5 @@ function onUrlChange() {
 }
 
 onUrlChange();
+
+
