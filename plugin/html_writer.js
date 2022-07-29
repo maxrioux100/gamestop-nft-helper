5a59dd5d3d46805ce6e16a5e6edd828219f65f10
Maxim Rioux <maxrioux100@gmail.com>
2022-07-29 11:11:08 -0400

    createHistoryHelper

--- plugin/html_writer.js
+++ plugin/html_writer.js
@@ -29,3 +29,26 @@ function createOffersHelperContainer() {
 	div.setAttribute('class', 'ContentContainerDesktop-sc-1p3n06p-5 eVGMue');
 	container.appendChild(div);
 }
+
+function createHistoryHelper(total_eth, values_eth, total_dollars, values_dollars, change) {
+	let container = document.getElementsByClassName("ContentContainer-sc-1p3n06p-4")[0];
+	let div = document.createElement('div');
+
+	div.innerHTML = '<header class="SectionTitle-sc-13gqei4-5 hiQCYL">' +
+						'<p class="sc-bkkeKt vhTUk">History helper</p>' +
+					'</header>' +
+					'<section class="Details-sc-asex48-0 ceZikd">' +
+						writeChip('Average', `${bestRound(total_eth/values_eth.length, 4)} ETH ($${bestRound(total_dollars/values_eth.length, 2)})`) +
+						writeChip('Median', `${median(values_eth)} ETH ($${median(values_dollars)})`) +
+						writeChip('Standard deviation', `${bestRound(getStandardDeviation(values_eth), 4)} ETH ($${bestRound(getStandardDeviation(values_dollars), 2)})`) +
+						writeChip('Change', `${bestRound(change, 2)}%`) +
+					'</section>'+
+					'<section>' +
+						'<div id="chart"></div>' +
+						'<div id="chart3"></div>' +
+						'<div id="chart4"></div>' +
+					'</section>';
+	div.setAttribute('id', 'history_helper');
+	div.setAttribute('class', 'ContentContainerDesktop-sc-1p3n06p-5 eVGMue');
+	container.appendChild(div);
+}
