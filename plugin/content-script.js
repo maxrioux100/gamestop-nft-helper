3416032ef3fef9fa67db786d971384b100c374ef
Maxim Rioux <maxrioux100@gmail.com>
2022-07-29 11:21:20 -0400

    storage.js

--- plugin/content-script.js
+++ plugin/content-script.js
@@ -169,18 +169,6 @@ function updateHistory(histories, transactions) {
 	}
 }
 
-function persistProfileName(tempProfileName) {
-	chrome.storage.local.set({profileName: tempProfileName}, function() {
-		profileName = tempProfileName;
-	});
-}
-
-function getProfileName() {
-	chrome.storage.local.get(['profileName'], function(_profileName) {
-		profileName = _profileName.profileName;
-	});
-}
-
 let nft;
 
 async function updateHistoryWithApiData() {
---
+++ plugin/storage.js
@@ -0,0 +1,11 @@
+function persistProfileName(tempProfileName) {
+	chrome.storage.local.set({profileName: tempProfileName}, function() {
+		profileName = tempProfileName;
+	});
+}
+
+function getProfileName() {
+	chrome.storage.local.get(['profileName'], function(_profileName) {
+		profileName = _profileName.profileName;
+	});
+}
