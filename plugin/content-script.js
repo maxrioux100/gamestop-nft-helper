5bc22fef9008d9fc871ebf4dd97e08b0c4de217f
Maxim Rioux <maxrioux100@gmail.com>
2022-07-29 09:52:48 -0400

    moved writeChip to html_writer

--- plugin/content-script.js
+++ plugin/content-script.js
@@ -3,17 +3,6 @@ var chart2;
 var chart3;
 var chart4;
 
-function writeChip(name, value){
-	return 	'<div class="DetailsItem-sc-asex48-1 gyMyxn">' +
-				'<div class="sc-jcFjpl ezpidm">' +
-					'<p class="sc-iUKqMP fqgUlr">' +
-						`<span class="sc-iAKWXU dCLfEr">${name}</span>` +
-						`<span class="sc-iAKWXU sc-efQSVx dCLfEr csRHYK">${value}</span>` +
-					'</p>' +
-				'</div>' +
-			'</div>';
-}
-
 function createOffersHelperContainer() {
   let editions = document.querySelectorAll("[class^='ButtonHoverWrapper']")[1];
   if (editions == undefined) {
---
+++ plugin/html_writer.js
@@ -0,0 +1,10 @@
+function writeChip(name, value){
+	return 	'<div class="DetailsItem-sc-asex48-1 gyMyxn">' +
+				'<div class="sc-jcFjpl ezpidm">' +
+					'<p class="sc-iUKqMP fqgUlr">' +
+						`<span class="sc-iAKWXU dCLfEr">${name}</span>` +
+						`<span class="sc-iAKWXU sc-efQSVx dCLfEr csRHYK">${value}</span>` +
+					'</p>' +
+				'</div>' +
+			'</div>';
+}
