3e9b5f19e96adb7c450c5bcae4a6149a7eb6999f
Maxim Rioux <maxrioux100@gmail.com>
2022-08-02 12:48:11 -0400

    shrinked menu.html

--- plugin/menu.js
+++ plugin/menu.js
@@ -4,26 +4,29 @@ menu['MoveTools'] = 'Move the toolbar to the left side';
 menu['StickMenu'] = 'Stick the menu bar to the page';
 
 for (let key in menu) {
+	let container = document.body;
 	let div = document.createElement('div');
+	
+	div.innerHTML = `<input class="form-check-input" type="checkbox" role="switch" id="${key}" checked />` +
+					`<label class="form-check-label" for="${key}">${menu[key]}</label>`;
+	
+	div.setAttribute('class', 'form-check form-switch');
+	container.appendChild(div);
 }
 
-//		<div class="form-check form-switch">
-//			<input class="form-check-input" type="checkbox" role="switch" id="MovePrice" checked />
-//			<label class="form-check-label" for="MovePrice">Move the price interface to the left side</label>
-//		</div>
-
-
-const boxes = document.getElementsByClassName('form-check-input');
-
-async function updateDefaultConfig(){
+async function updateDefaultConfig(boxes){
 	await readPreferences();
-	for (let box of boxes) {
+	for (let box of boxes) {	
 		box.checked = preferences[box.id];
 		box.addEventListener('change', (e) => { persistPreferences(e.target.id, e.target.checked); });
 	}
 }
 
-updateDefaultConfig()
+waitForElement(".form-check-input", 1000)
+.then( () => {
+	const boxes = document.getElementsByClassName('form-check-input');
+	updateDefaultConfig(boxes);
+});
 
 
 
