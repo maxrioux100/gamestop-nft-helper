f05af7def8fe8a60a153b4ac791d0b77eacc3f74
Maxim Rioux <maxrioux100@gmail.com>
2022-08-02 12:31:34 -0400

    Update menu.js

--- plugin/menu.js
+++ plugin/menu.js
@@ -1,3 +1,18 @@
+var menu = {};
+menu['MovePrice'] = 'Move the price interface to the left side';
+menu['MoveTools'] = 'Move the toolbar to the left side'; 
+menu['StickMenu'] = 'Stick the menu bar to the page';
+
+for (let key in menu) {
+	let div = document.createElement('div');
+}
+
+//		<div class="form-check form-switch">
+//			<input class="form-check-input" type="checkbox" role="switch" id="MovePrice" checked />
+//			<label class="form-check-label" for="MovePrice">Move the price interface to the left side</label>
+//		</div>
+
+
 const boxes = document.getElementsByClassName('form-check-input');
 
 async function updateDefaultConfig(){
