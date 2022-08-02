07ea7cd8b85cd8ec7a4766ff4f373dc657a046c0
Maxim Rioux <maxrioux100@gmail.com>
2022-08-02 12:13:33 -0400

    shrinked menu.js

--- plugin/menu.js
+++ plugin/menu.js
@@ -1,21 +1,15 @@
-const movePrice = document.getElementById('MovePrice');
-const moveTools = document.getElementById('MoveTools');
-const stickMenu = document.getElementById('StickMenu');
-
 const boxes = document.getElementsByClassName('form-check-input');
-for (let box of boxes) {
-	box.addEventListener('change', (e) => {
-		let dict = {};
-		dict[e.target.id] = e.target.checked;
-		persistPreferences(dict);
-	});
-}
 
 async function updateDefaultConfig(){
 	await readPreferences();
-	movePrice.checked = preferences['MovePrice'];
-	moveTools.checked = preferences['MoveTools'];
-	stickMenu.checked = preferences['StickMenu'];
+	for (let box of boxes) {
+		box.checked = preferences[box.id];
+		box.addEventListener('change', (e) => {
+			let dict = {};
+			dict[e.target.id] = e.target.checked;
+			persistPreferences(dict);
+		});
+	}
 }
 
 updateDefaultConfig()
