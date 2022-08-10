4380480684bf70d4b64bb80e93b4a49cc70ddfa5
maxrioux100 <40430512+maxrioux100@users.noreply.github.com>
2022-08-10 11:40:05 -0400

    1.2.8-when-the-purchase-is-moved-the-edititons-btn-interface-is-laggy-as-fuck (#145)
    * Update manifest.json
    
    * save but no background
    
    * working great
    
    * Update utility.js

--- plugin/utility.js
+++ plugin/utility.js
@@ -108,7 +108,11 @@ function updateDark() {
 	document.body.style.backgroundColor = '#424242';
 	document.querySelector('.FooterOuterWrapper-sc-n1m1px-6').style.backgroundColor = '#424242'; 
 	document.querySelector('.sc-lkgTHX').src = 'https://nft.gamestop.com/7c4d1a56a3e70a618ca864a2feb58fcd.svg';
-
+	if (preferences['MovePrice']) {
+		document.querySelector('.InfoBoxInnerWrapper-sc-11cpe2k-2').style.backgroundColor = '#FBFBFB'
+		document.querySelector('.InfoBoxInnerWrapper-sc-11cpe2k-2').style.backgroundClip = 'content-box';
+		document.querySelector('.PurchaseInfoWrapper-sc-11cpe2k-0').firstChild.style.display = 'none';
+	}
 	let whitefont = document.querySelectorAll('*:not(.sc-gsDKAQ,.sc-dkPtRN.vygPD,.mask,img)');
 	for (_element of whitefont){
 		if (_element.style) { _element.style.color = '#FBFBFB'; }
@@ -235,14 +239,14 @@ async function moveThings(){
 		}
 		
 		if (preferences['MovePrice']) { 
-			moveThing('PurchaseInfoWrapper-sc-11cpe2k-0', 'MediaContainer-sc-1p3n06p-2', buttons = [
-																									['div div .InfoBoxInnerWrapper-sc-11cpe2k-2 .InnerButtonWrapper-sc-11cpe2k-3 .MultiButtonRow-sc-11cpe2k-5 .ButtonHoverWrapper-sc-11cpe2k-4 .jhCfFf'],
-																									['div div .InfoBoxInnerWrapper-sc-11cpe2k-2 .InnerButtonWrapper-sc-11cpe2k-3 .MultiButtonRow-sc-11cpe2k-5 .ButtonHoverWrapper-sc-11cpe2k-4 .vygPD']
+			moveThing('InfoBoxInnerWrapper-sc-11cpe2k-2', 'MediaContainer-sc-1p3n06p-2', buttons = [
+																									['.InnerButtonWrapper-sc-11cpe2k-3 .MultiButtonRow-sc-11cpe2k-5 .ButtonHoverWrapper-sc-11cpe2k-4 .jhCfFf'],
+																									['.InnerButtonWrapper-sc-11cpe2k-3 .MultiButtonRow-sc-11cpe2k-5 .ButtonHoverWrapper-sc-11cpe2k-4 .vygPD']
 																								   ], where='end', paddingTop=20); 																					   
-			waitForElement('.MediaContainer-sc-1p3n06p-2 .PurchaseInfoWrapper-sc-11cpe2k-0 div div .InfoBoxInnerWrapper-sc-11cpe2k-2 .InnerButtonWrapper-sc-11cpe2k-3 .ActionButtonAlignRight-sc-1jv4yz7-0', 1000)
+			waitForElement('.MediaContainer-sc-1p3n06p-2 .InfoBoxInnerWrapper-sc-11cpe2k-2 .InnerButtonWrapper-sc-11cpe2k-3 .ActionButtonAlignRight-sc-1jv4yz7-0', 1000)
 			.then(() => {	
 
-				let actions = document.querySelectorAll('.MediaContainer-sc-1p3n06p-2 .PurchaseInfoWrapper-sc-11cpe2k-0 div div .InfoBoxInnerWrapper-sc-11cpe2k-2 .InnerButtonWrapper-sc-11cpe2k-3 .ActionButtonAlignRight-sc-1jv4yz7-0');
+				let actions = document.querySelectorAll('.MediaContainer-sc-1p3n06p-2 .InfoBoxInnerWrapper-sc-11cpe2k-2 .InnerButtonWrapper-sc-11cpe2k-3 .ActionButtonAlignRight-sc-1jv4yz7-0');
 				
 				for (let i = 0 ; i < actions.length ; i++){
 					let nbEditions = '';
@@ -264,21 +268,21 @@ async function moveThings(){
 											`</div>` +
 										  `</div>`;
 										  
-					let action_btn = document.querySelector(`.ContentContainerDesktop-sc-1p3n06p-5 .PurchaseInfoWrapper-sc-11cpe2k-0 div div .InfoBoxInnerWrapper-sc-11cpe2k-2 .InnerButtonWrapper-sc-11cpe2k-3 .ActionButtonAlignRight-sc-1jv4yz7-0 .sc-eCImPb .sc-dkPtRN`);
-					let manage_btn = document.querySelector(`.MediaContainer-sc-1p3n06p-2 .PurchaseInfoWrapper-sc-11cpe2k-0 div div .InfoBoxInnerWrapper-sc-11cpe2k-2 .InnerButtonWrapper-sc-11cpe2k-3 .MultiButtonRow-sc-11cpe2k-5 .ButtonHoverWrapper-sc-11cpe2k-4 .jhCfFf`);
+					let action_btn = document.querySelector(`.ContentContainerDesktop-sc-1p3n06p-5 .InfoBoxInnerWrapper-sc-11cpe2k-2 .InnerButtonWrapper-sc-11cpe2k-3 .ActionButtonAlignRight-sc-1jv4yz7-0 .sc-eCImPb .sc-dkPtRN`);
+					let manage_btn = document.querySelector(`.MediaContainer-sc-1p3n06p-2 .InfoBoxInnerWrapper-sc-11cpe2k-2 .InnerButtonWrapper-sc-11cpe2k-3 .MultiButtonRow-sc-11cpe2k-5 .ButtonHoverWrapper-sc-11cpe2k-4 .jhCfFf`);
 					if (manage_btn) {
 						manage_btn.addEventListener("click", (e) => {
 							action_btn.click();
-							let manage_menu = document.querySelector(`.ContentContainerDesktop-sc-1p3n06p-5 .PurchaseInfoWrapper-sc-11cpe2k-0 div div .InfoBoxInnerWrapper-sc-11cpe2k-2 .InnerButtonWrapper-sc-11cpe2k-3 .ActionButtonAlignRight-sc-1jv4yz7-0 .sc-eCImPb .sc-iCfMLu`);
+							let manage_menu = document.querySelector(`.ContentContainerDesktop-sc-1p3n06p-5 .InfoBoxInnerWrapper-sc-11cpe2k-2 .InnerButtonWrapper-sc-11cpe2k-3 .ActionButtonAlignRight-sc-1jv4yz7-0 .sc-eCImPb .sc-iCfMLu`);
 							manage_menu.children[1].click();
 						});
 					}
 					
-					let editions_btn = document.querySelector(`.MediaContainer-sc-1p3n06p-2 .PurchaseInfoWrapper-sc-11cpe2k-0 div div .InfoBoxInnerWrapper-sc-11cpe2k-2 .InnerButtonWrapper-sc-11cpe2k-3 .MultiButtonRow-sc-11cpe2k-5 .ButtonHoverWrapper-sc-11cpe2k-4 .vygPD`);
+					let editions_btn = document.querySelector(`.MediaContainer-sc-1p3n06p-2 .InfoBoxInnerWrapper-sc-11cpe2k-2 .InnerButtonWrapper-sc-11cpe2k-3 .MultiButtonRow-sc-11cpe2k-5 .ButtonHoverWrapper-sc-11cpe2k-4 .vygPD`);
 					if (editions_btn) {
 						editions_btn.addEventListener("click", (e) => {
 							action_btn.click();
-							let manage_menu = document.querySelector(`.ContentContainerDesktop-sc-1p3n06p-5 .PurchaseInfoWrapper-sc-11cpe2k-0 div div .InfoBoxInnerWrapper-sc-11cpe2k-2 .InnerButtonWrapper-sc-11cpe2k-3 .ActionButtonAlignRight-sc-1jv4yz7-0 .sc-eCImPb .sc-iCfMLu`);
+							let manage_menu = document.querySelector(`.ContentContainerDesktop-sc-1p3n06p-5 .InfoBoxInnerWrapper-sc-11cpe2k-2 .InnerButtonWrapper-sc-11cpe2k-3 .ActionButtonAlignRight-sc-1jv4yz7-0 .sc-eCImPb .sc-iCfMLu`);
 							manage_menu.children[0].click();
 						});
 					}
