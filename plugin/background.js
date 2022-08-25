8dce9cda1aaa7e1141d7efbc720092990298f4ce
Maxim Rioux <maxrioux100@gmail.com>
2022-08-25 15:49:44 -0400

    sending api request

--- plugin/background.js
+++ plugin/background.js
@@ -1,8 +1,11 @@
 //background.js
-setTimeout(() => {  
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-	  chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-		console.log(response.farewell);
-	  });
-	});
- }, 5000);
+console.log('testout');
+chrome.webRequest.onCompleted.addListener(
+	function(details) {
+		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+			chrome.tabs.sendMessage(tabs[0].id, {greeting: details.url});
+		});
+	},
+	{ urls: ["https://api.nft.gamestop.com/nft-svc-marketplace/*", "https://nft.gamestop.com/*"] }
+);
+
--- plugin/content-script.js
+++ plugin/content-script.js
@@ -392,11 +392,7 @@ async function profile_page() {
 }
 
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension");
-    if (request.greeting == "hello")
-      sendResponse({farewell: "goodbye"});
-  }
+	function(request, sender, sendResponse) {
+		console.log(request.greeting );
+	}
 );
