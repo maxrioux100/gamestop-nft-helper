fb7276e802e71015d00bb33ccd9df817d9157151
Maxim Rioux <maxrioux100@gmail.com>
2022-08-25 16:04:32 -0400

    Update background.js

--- plugin/background.js
+++ plugin/background.js
@@ -1,9 +1,7 @@
-//background.js
-console.log('testout');
 chrome.webRequest.onCompleted.addListener(
 	function(details) {
 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-			chrome.tabs.sendMessage(tabs[0].id, {greeting: details.url});
+			chrome.tabs.sendMessage(details.tabId, {greeting: details.url});
 		});
 	},
 	{ urls: ["https://api.nft.gamestop.com/nft-svc-marketplace/*"] }
