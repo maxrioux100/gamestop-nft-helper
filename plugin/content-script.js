a1d90890d06ce246ce4fc4cbf2f8bc8de2c1b26b
Maxim Rioux <maxrioux100@gmail.com>
2022-08-25 16:46:46 -0400

    Update content-script.js

--- plugin/content-script.js
+++ plugin/content-script.js
@@ -393,6 +393,6 @@ async function profile_page() {
 
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
-		console.log(request.greeting );
+		console.log(request.data );
 	}
 );
