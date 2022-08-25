98363f7c7bab911c4aea3e8deb5b202b56b04c85
Maxim Rioux <maxrioux100@gmail.com>
2022-08-25 14:59:34 -0400

    test message working

---
+++ plugin/background.js
@@ -0,0 +1,8 @@
+//background.js
+setTimeout(() => {  
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	  chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+		console.log(response.farewell);
+	  });
+	});
+ }, 5000);
--- plugin/content-script.js
+++ plugin/content-script.js
@@ -390,3 +390,13 @@ async function profile_page() {
 		});
 	}, 3000);
 }
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    if (request.greeting == "hello")
+      sendResponse({farewell: "goodbye"});
+  }
+);
